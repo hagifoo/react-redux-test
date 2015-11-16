d18d159697293b158ad89e73b2eda0594cfbe96c
hagihara <hagifoo@balus.me>
2015-11-16 23:39:05 +0900

    [add] Add functionalities for voting.

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
 
-const pair = ['Trainspotting2', '28 Days Later'];
+const pair = ['Trainspotting', '28 Days Later'];
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Voting pair={pair} winner="Trainspotting" />,
   document.getElementById('app')
 );
